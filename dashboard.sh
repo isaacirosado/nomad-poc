19912ae36981f84ea456d0753e1904df573b8a18
Isaac Rosado <isaac.israel.rosado@gmail.com>
2021-11-04 16:35:38 +0000

    Exposing dashboards, password-protected mind-you

---
+++ dashboard.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Example of manual service registrations to Consul with proper tagging so the load-balancer will pick them up
+set -e
+set -o errexit
+set -o nounset
+set -o pipefail
+
+consul services register -port=4646 -name=nomad-dashboard -tag="traefik.enable=true" -tag='traefik.http.routers.nomad-dashboard.rule=Host(`nomad.rosado.live`)' -tag="traefik.http.routers.nomad-dashboard.middlewares=auth" -tag='traefik.http.middlewares.auth.basicauth.users=ghost:$apr1$x7.AV8Ov$sUu7JOkV9yoKIXkI3biBq.'
+
+consul services register -port=8500 -name=consul-dashboard -tag="traefik.enable=true" -tag='traefik.http.routers.consul-dashboard.rule=Host(`consul.rosado.live`)' -tag="traefik.http.routers.consul-dashboard.middlewares=auth" -tag='traefik.http.middlewares.auth.basicauth.users=ghost:$apr1$x7.AV8Ov$sUu7JOkV9yoKIXkI3biBq.'
--- modules/cluster-client/consul.hcl
+++ modules/cluster-client/consul.hcl
@@ -8,3 +8,6 @@ retry_join = ["provider=digitalocean region=${dc} tag_name=server api_token=${to
 limits {
   http_max_conns_per_client = 1200
 }
+ui_config {
+  enabled = true
+}
